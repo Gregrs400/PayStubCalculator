152320b98896ec4342462ee9e79b1ef63d151ae3
Greg Salisbury <gregsalisbury25@gmail.com>
2025-05-31 18:35:40 -0400

    Declared and assigned user input variables

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,5 +1,35 @@
+import java.util.Scanner;
+
 public class Main
 {
-    public static void main(String[] args) {
+    public static void main(String[] args)
+    {
+
+        Scanner keyboard = new Scanner(System.in);
+
+        String payPeriodStartDate, payPeriodEndDate;
+        double grossPay;
+
+        // user input
+
+            // pay period start date (mm/dd/yyyy)
+
+        System.out.print("Enter pay period start date (mm/dd/yyyy): ");
+
+        payPeriodStartDate = keyboard.nextLine();
+
+            // pay period end date (mm/dd/yyyy)
+
+        System.out.print("Enter pay period end date (mm/dd/yyyy): ");
+
+        payPeriodEndDate = keyboard.nextLine();
+
+            // gross pay
+
+        System.out.print("Enter gross pay: ");
+
+        grossPay = keyboard.nextDouble();
+
+        // create csv of pay stub
     }
 }
