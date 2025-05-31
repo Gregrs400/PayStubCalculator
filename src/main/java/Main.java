c63beb613e2ad607d94b53439485f678c852e5ef
Greg Salisbury <gregsalisbury25@gmail.com>
2025-05-31 18:38:29 -0400

    Added print statements for testing.

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -18,17 +18,23 @@ public class Main
 
         payPeriodStartDate = keyboard.nextLine();
 
+        System.out.println(payPeriodStartDate);
+
             // pay period end date (mm/dd/yyyy)
 
         System.out.print("Enter pay period end date (mm/dd/yyyy): ");
 
         payPeriodEndDate = keyboard.nextLine();
 
+        System.out.println(payPeriodEndDate);
+
             // gross pay
 
         System.out.print("Enter gross pay: ");
 
         grossPay = keyboard.nextDouble();
+
+        System.out.printf("$%.2f", grossPay);
 
         // create csv of pay stub
     }
